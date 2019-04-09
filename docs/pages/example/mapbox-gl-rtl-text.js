a153878a051f9acb96e0484255ce334bc973b714
samanpwbb <samanpwbb@gmail.com>
2019-04-08 18:34:09 -0700

    Studio does support rtl plugin

--- docs/pages/example/mapbox-gl-rtl-text.js
+++ docs/pages/example/mapbox-gl-rtl-text.js
@@ -3,7 +3,7 @@ title: Add support for right-to-left scripts
 description: >-
   Use the [mapbox-gl-rtl-text](https://github.com/mapbox/mapbox-gl-rtl-text)
   plugin to support scripts that use right-to-left layout (such as Arabic or
-  Hebrew). Note that Mapbox Studio does not yet support RTL text.
+  Hebrew).
 tags:
   - internationalization
 pathname: /mapbox-gl-js/example/mapbox-gl-rtl-text/
